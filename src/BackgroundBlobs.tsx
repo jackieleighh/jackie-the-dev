98b62ab14cf0c8e1ce0b7cec9d3f0bf98796405d
Jacqueline Harnish <jacquelineharnish@mac.mynetworksettings.com>
2026-03-16 12:50:41 -0400

    lava lamp

--- src/BackgroundBlobs.tsx
+++ src/BackgroundBlobs.tsx
@@ -6,6 +6,30 @@ function BackgroundBlobs() {
       <div className="blob blob-pink" />
       <div className="blob blob-lime" />
       <div className="blob blob-orange" />
+      <div className="lava-bubbles">
+        <svg style={{ position: 'absolute', width: 0, height: 0 }}>
+          <defs>
+            <filter id="lava">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="14" result="blur" />
+              <feColorMatrix in="blur" type="matrix"
+                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 22 -9" />
+            </filter>
+          </defs>
+        </svg>
+        <div className="lava-bubble-layer">
+          <div className="lava-pool" />
+          <div className="lava-blob lava-blob-1" />
+          <div className="lava-blob lava-blob-2" />
+          <div className="lava-blob lava-blob-3" />
+          <div className="lava-blob lava-blob-4" />
+          <div className="lava-blob lava-blob-5" />
+          <div className="lava-blob lava-blob-6" />
+          <div className="lava-blob lava-blob-7" />
+          <div className="lava-blob lava-blob-8" />
+          <div className="lava-blob lava-blob-9" />
+          <div className="lava-blob lava-blob-10" />
+        </div>
+      </div>
     </div>
   );
 }
